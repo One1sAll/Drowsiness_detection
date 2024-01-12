db8e5582c15414cd8b207d27c8968cad4ed24387
happy <2630391116@qq.com>
2024-01-12 16:15:50 +0800

    model.py补充一些注释

--- model.py
+++ model.py
@@ -1,12 +1,6 @@
-import os
 from keras.preprocessing import image
-import matplotlib.pyplot as plt 
-import numpy as np
-from keras.utils.np_utils import to_categorical
-import random,shutil
 from keras.models import Sequential
 from keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
-from keras.models import load_model
 
 # 生成数据集
 def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
@@ -17,42 +11,37 @@ BS= 32 # batch size
 TS=(24,24) # target size
 train_batch= generator('data/train',shuffle=True, batch_size=BS,target_size=TS)
 valid_batch= generator('data/valid',shuffle=True, batch_size=BS,target_size=TS)
-# epoch数；train_batch.classes获取labels
+# 每个epoch的步数；train_batch.classes获取labels
 SPE= len(train_batch.classes)//BS
 VS = len(valid_batch.classes)//BS
 print(SPE,VS)
 
-
-# img,labels= next(train_batch)
-# print(img.shape)
-
+# 构建模型
 model = Sequential([
-    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(24,24,1)),
-    MaxPooling2D(pool_size=(1,1)),
+    # 第一层 32个卷积滤波器 每个大小3×3
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(24,24,1)),   # 第一层卷积层
+    MaxPooling2D(pool_size=(1,1)),  # 第一层池化层
+
+    # 第二层 32个卷积滤波器 每个大小3×3
     Conv2D(32,(3,3),activation='relu'),
     MaxPooling2D(pool_size=(1,1)),
-#32 convolution filters used each of size 3x3
-#again
+
+    # 第三层 64个卷积滤波器 每个大小 3x3
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(pool_size=(1,1)),
 
-#64 convolution filters used each of size 3x3
-#choose the best features via pooling
-    
-#randomly turn neurons on and off to improve convergence
-    Dropout(0.25),
-#flatten since too many dimensions, we only want a classification output
-    Flatten(),
-#fully connected to get all relevant data
-    Dense(128, activation='relu'),
-#one more dropout for convergence' sake :) 
-    Dropout(0.5),
-#output a softmax to squash the matrix into output probabilities
+    # 全连接层
+    Dropout(0.25), # 随机失活
+    Flatten(), # 将多维输入一维化
+    Dense(128, activation='relu'), # 全连接层
+    Dropout(0.5), # 随机失活使模型更好地收敛
+
+    # 输出层
     Dense(2, activation='softmax')
 ])
-
+# 编译模型
 model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
-
+# 使用生成器进行模型训练
 model.fit_generator(train_batch, validation_data=valid_batch,epochs=15,steps_per_epoch=SPE ,validation_steps=VS)
-
+# 保存模型
 model.save('models/cnnCat2.h5', overwrite=True)
